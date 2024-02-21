4c33a3d4b0842225a5009cb6a8bd5823799663ed
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-21 15:13:49 -0300

    feat: Iniciado o projeto do desafio PicPay

---
+++ src/main/java/com/picpay/challenge/DesafioPicPayApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPicPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpay/challenge/DesafioPicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
