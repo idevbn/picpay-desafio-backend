f79e898263957e58448b6425a2216b8cbcdffc8e
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-21 20:52:13 -0300

    chore: Adicionada migration que cria tabelas tb_users e tb_transactions

---
+++ src/main/resources/db/migrations/V001__cria-tabelas-de-users-e-transactions.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tb_users (
+  id SERIAL PRIMARY KEY,
+  firstName VARCHAR(50) NOT NULL,
+  lastName VARCHAR(255) NOT NULL,
+  cpf VARCHAR(11) UNIQUE NOT NULL,
+  email VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  balance NUMERIC(12, 2) NOT NULL,
+  userType VARCHAR(30) NOT NULL DEFAULT 'REGULAR'
+);
+
+
+CREATE TABLE tb_transactions (
+  id SERIAL PRIMARY KEY,
+  amount NUMERIC(12, 2) NOT NULL,
+  sender VARCHAR(50) NOT NULL,
+  receiver VARCHAR(50) NOT NULL,
+  timestamp TIMESTAMP WITHOUT TIME ZONE,
+  CONSTRAINT fkey_transactions_users FOREIGN KEY(id) REFERENCES tb_users(id)
+);
