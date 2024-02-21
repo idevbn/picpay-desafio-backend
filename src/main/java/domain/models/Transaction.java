a2652c178a3afd5522842a654cd1210420d0f082
Idevaldo Neto <inetomec@yahoo.com.br>
2024-02-21 18:24:00 -0300

    feat: Criadas entidades User e Transaction

---
+++ src/main/java/domain/models/Transaction.java
@@ -0,0 +1,29 @@
+package domain.models;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity(name = "transactions")
+@Table(name = "tb_transactions")
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/domain/models/User.java
@@ -0,0 +1,32 @@
+package domain.models;
+
+import domain.enums.UserType;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity(name = "users")
+@Table(name = "tb_users")
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String cpf;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(value = EnumType.STRING)
+    private UserType userType;
+
+}
