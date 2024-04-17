8e5e05846e59ff46180da071f026c35c4cb0c934
Idevaldo Neto <inetomec@yahoo.com.br>
2024-04-17 14:08:34 -0300

    test: Criado script de inserção de um usuário

---
+++ src/test/resources/database/user/create_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_users(id, first_name, last_name, cpf, email, password, balance, user_type)
+VALUES(1, 'JOHN', 'DOE', '80497677440', 'john_doe@email.com', '$2a$10$f7iS6mINRZKIWkSHY7wZQuVZCSZQNUGkNtHQmiqogr0pUUbAOG71q', 100.00, 'REGULAR');
