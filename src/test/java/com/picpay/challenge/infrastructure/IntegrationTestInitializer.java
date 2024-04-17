75e91a1b40aeaddbaea816e45aaeb9f58c005164
Idevaldo Neto <inetomec@yahoo.com.br>
2024-04-17 14:04:03 -0300

    test: Injetada dependência do TestRestTemplate
    As classes de teste, que herdarão diretamente a IntegrationTestInitializer, farão requests com a classe injetada

--- src/test/java/com/picpay/challenge/infrastructure/IntegrationTestInitializer.java
+++ src/test/java/com/picpay/challenge/infrastructure/IntegrationTestInitializer.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,6 +18,9 @@ public class IntegrationTestInitializer {
     @ClassRule
     public static PostgresTestContainer postgresContainerConfig
             = PostgresTestContainer.getInstance();
+
+    @Autowired
+    protected TestRestTemplate testRestTemplate;
 
     @Test
     public void simpleAssertIntegrationTestInitializer() {
